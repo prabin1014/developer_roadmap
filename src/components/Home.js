136438ff776445f4032a94ce7defa053d54fb2be
Prabin Tamang <prabint1014@gmail.com>
2023-06-21 23:32:26 +0545

    added the links that direct to details of particular roadmap

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,35 @@ export const Home = () => {
           effort to guide them a path with overall contents.
         </p>
       </div>
+
+      <section className="roadmap-content">
+        <div className="line"></div>
+        <div className="roadmap-content-title">Role Based Roadmaps</div>
+        <div className="line"></div>
+      </section>
+
+      <section className="roadmap-field">
+        <ul className="roadmap-field-list">
+          <li className="roadmap-field-list-span">
+            <span> Frontend </span>
+          </li>
+          <li className="roadmap-field-list-span">
+            <span> Backend</span>
+          </li>
+          <li className="roadmap-field-list-span">
+            <span> Database </span>
+          </li>
+          <li className="roadmap-field-list-span">
+            <span> UI/UX Design </span>
+          </li>
+          <li className="roadmap-field-list-span">
+            <span> Cyber Security </span>
+          </li>
+          <li className="roadmap-field-list-span">
+            <span> Presentation </span>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
