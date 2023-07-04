b48a2fad3443b718741b10ee9791470f99561e96
Prabin Tamang <prabint1014@gmail.com>
2023-07-04 11:15:42 +0545

    add home route in logo

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as MyIcon } from "../../assets/roadmap.svg";
+import { NavLink } from "react-router-dom";
 import "./TopNav.scss";
 
 export const TopNav = () => {
@@ -7,9 +8,11 @@ export const TopNav = () => {
     <>
       <nav className="topnav">
         <ul className="topnav-link">
-          <li className="topnav-link-item">
-            <MyIcon fill="white" />
-          </li>
+          <NavLink to="/">
+            <li className="topnav-link-item">
+              <MyIcon fill="white" />
+            </li>
+          </NavLink>
           <li className="topnav-link-item"> Roadmaps </li>
           <li className="topnav-link-item"> Best Practices</li>
           <li className="topnav-link-item"> Guidance</li>
